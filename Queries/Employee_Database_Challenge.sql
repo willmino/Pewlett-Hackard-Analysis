a8c9c4389855f5ae73db6dda14966dc9f4d7c257
willmino <will.mino@yahoo.com>
2023-01-06 10:26:47 -0800

    uploading files

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,59 @@
+--Deliverable 1
+
+--retirement_titles
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees as e
+JOIN titles as t ON e.emp_no = t.emp_no
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no ASC;
+
+SELECT * FROM retirement_titles
+
+
+--unique_titles
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name, 
+title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE to_date = '9999-01-01'
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
+
+SELECT * FROM unique_titles
+
+
+--retiring_titles
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT DESC;
+
+SELECT * FROM retiring_titles
+
+
+--Deliverable 2
+-- Write a query to create a Mentorship Eligibility table that holds the employees
+-- who are eligible to participate in a mentorship program.
+
+--men
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+	de.from_date, de.to_date,
+	t.title
+INTO membership_eligibility
+FROM employees as e
+JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE de.to_date = '9999-01-01' AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY emp_no ASC;
+
+SELECT * FROM membership_eligibility
+
+
+
+
+
